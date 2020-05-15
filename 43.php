2f1d8bb605ee67141f00036af29a433d05acef22
xwojtekx99 <62295611+xwojtekx99@users.noreply.github.com>
2020-05-15 14:28:58 +0200

    Update 43.php

--- 43.php
+++ 43.php
@@ -13,7 +13,7 @@
     $db_name = "uczelnia";
         
     $dbh = new PDO("mysql:dbname=$db_name;host=$host",$db_user,$db_password);
-    $sql = $dbh->exec("SELECT studenci.imie, studenci.nazwisko, rok.kierunek, rok.stopien FROM studenci INNER JOIN rok ON studenci.id_rok_studiow=rok.id;");
+    $sql = $dbh->query("SELECT studenci.imie, studenci.nazwisko, rok.kierunek, rok.stopien FROM studenci INNER JOIN rok ON studenci.id_rok_studiow=rok.id;");
       
 ?>
     
