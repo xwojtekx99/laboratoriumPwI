8ecae50bf14ea099f0c01cdc0db5eb5cbbe6deec
xwojtekx99 <62295611+xwojtekx99@users.noreply.github.com>
2020-05-15 14:16:57 +0200

    Update 40.php

--- 40.php
+++ 40.php
@@ -5,34 +5,22 @@
 </head>
 <body>
 <?php
-    session_start();
     
     $host = "localhost"; 
     $db_user = "root"; 
     $db_password = "";
     $db_name = "uczelnia";
         
+    $dbh = new PDO("mysql:dbname=$db_name;host=$host",$db_user,$db_password);
+    $sql = $dbh->exec("INSERT INTO studenci values(1, 'Alan', 'Kowalski', 'alanik@wp.pl', 1)");
+    $sql = $dbh->exec("INSERT INTO studenci values(2, 'Tomasz', 'Nowak', 'tt_nowk@wp.pl', 1)");
+    $sql = $dbh->exec("INSERT INTO studenci values(3, 'Robert', 'Juskowiak', 'robojk@wp.pl', 2)");
+    $sql = $dbh->exec("INSERT INTO rok values(1, 'Politechnika Warszawska', 'Matematyka', 'inzynier')");
+    $sql = $dbh->exec("INSERT INTO rok values(2, 'Uniwersytet Jagiellonski', 'Teologia', 'licencjat')");
     
-    $connection = new mysqli($host, $db_user, $db_password,$db_name)
-    or die('Brak połączenia z serwerem MySQL');
     
-    $ins = "INSERT INTO studenci values(1, 'Alan', 'Kowalski', 'alanik@wp.pl', 1)";
-    $ins1 = "INSERT INTO studenci values(2, 'Marek', 'Nowak', 'nowakM1@wp.pl', 1)";
-    $ins2 = "INSERT INTO studenci values(3, 'Wiktor', 'Komor', 'wwwkomor@o2.pl', 2)";
-    $ins3 = "INSERT INTO rok values(1, 'Politechnika Warszawska', 'Matematyka', 'licencjat')";
-    $ins4 = "INSERT INTO rok values(2, 'Uniwersytet Jagiellonski', 'Prawo', 'magistrat')";
-    
-    @$connection->query($ins);
-    @$connection->query($ins1);
-    @$connection->query($ins2);
-    @$connection->query($ins3);
-    @$connection->query($ins4);
-    
-      
-    $connection->close();
-    
+        
 ?>
-    
-    
+        
 </body>
 </html>
